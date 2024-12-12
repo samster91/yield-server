7d1a8ae095982cbd9c5991763064f894d559265f
Samster91 <samster91>
2024-12-12 19:10:49 +0100

    FIX: Idle Api-v2 migration

--- src/adaptors/idle/index.js
+++ src/adaptors/idle/index.js
@@ -1,15 +1,10 @@
 const utils = require('../utils');
+const BN = require('bignumber.js');
 const superagent = require('superagent');
 
-const chains = {
-  ethereum: 'https://api.idle.finance/pools',
-  polygon: 'https://api-polygon.idle.finance/pools',
-  optimism: 'https://api-optimism.idle.finance/pools',
-  polygon_zkevm: 'https://api-zkevm.idle.finance/pools',
-};
+const endpoint = 'https://api-v2.idle.finance/v1/'
 
-const AUTH_TOKEN_ENCODED =
-  'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR2xsYm5SSlpDSTZJa0Z3Y0RZaUxDSnBZWFFpT2pFMk56QXlNemMxTWpkOS5rbnNtekVOSm40Yk5Ea0ZCM3h2eWZyaDBwVlFLTHY0NW9JanJQNHdRTU5N';
+const AUTH_TOKEN_ENCODED = 'ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SjFjMlZ5U1dRaU9pSTJOalJrWVRaak9ETTNOVEl4TURCaU5tTTNOakkyWW1FaUxDSjBiMnRsYmtsa0lqb2lOalk0Wm1JNFltWm1OamRsTldFM01XUmpaVEZsTnpWa0lpd2lhV0YwSWpveE56SXdOamswT1RjMWZRLlBzLU13RGZZSDNlZGl0QnhPLUJJQlFZXzJ5ckJZcGQ3M0QySTZBZFZ1SGc=';
 
 async function getDataWithAuth(url, token) {
   const data = await superagent
@@ -20,38 +15,57 @@ async function getDataWithAuth(url, token) {
 
 const getApy = async () => {
   const AUTH_TOKEN_DECODED = atob(AUTH_TOKEN_ENCODED);
-  const data = await Promise.allSettled(
-    Object.entries(chains).map(async (chain) => {
-      const data = await getDataWithAuth(chain[1], AUTH_TOKEN_DECODED);
-      return data.map((v) => {
-        let protocolName = v.protocolName;
-        if (v.borrowerName) {
-          protocolName += ` ${v.borrowerName}`;
-        }
-        const apyReward = v.apyReward || Number(0);
-        const rewardTokens = v.rewardTokens || [];
-        return {
-          pool: v.address,
-          apyBase: Number(v.apr),
-          apyReward,
-          rewardTokens,
-          symbol: v.tokenName,
-          poolMeta: v.poolName.includes('Best')
-            ? v.poolName.split(' ').slice(1).join(' ')
-            : `${protocolName} ${v.strategy}`,
-          tvlUsd: Number(v.tvl),
-          project: 'idle',
-          chain: utils.formatChain(chain[0]),
-          underlyingTokens: [v.underlyingAddress],
-        };
-      });
-    })
-  );
+  const [
+    vaults,
+    chains,
+    tokens,
+    vaultBlocks
+  ] = await Promise.all([
+    getDataWithAuth(`${endpoint}vaults`, AUTH_TOKEN_DECODED),
+    getDataWithAuth(`${endpoint}chains`, AUTH_TOKEN_DECODED),
+    getDataWithAuth(`${endpoint}tokens`, AUTH_TOKEN_DECODED),
+    getDataWithAuth(`${endpoint}vault-latest-blocks`, AUTH_TOKEN_DECODED)
+  ])
 
-  return data
-    .filter((i) => i.status === 'fulfilled')
-    .map((i) => i.value)
-    .flat();
+  // console.log('chains', chains.data)
+
+  const output = []
+  for await (v of vaults.data){
+    const apyReward = Number(0);
+    const rewardTokens = v.rewardTokens || [];
+  
+    const chain = chains.data.find( c => c._id === v.chainId )
+    const token = tokens.data.find( t => t._id === v.tokenId )
+    const vaultBlock = vaultBlocks.data.find( b => b.vaultId === v._id )
+
+    let apyBase = 0
+    const APR = vaultBlock.APRs.find( apr => apr.type === 'BASE' || apr.type === 'GROSS' )
+    if (!APR || APR.rate <= 0){
+      const vaultPerformance = await getDataWithAuth(`${endpoint}vault-performances?vaultBlockId=${vaultBlock._id}`, AUTH_TOKEN_DECODED)
+      if (vaultPerformance.data?.length){
+        apyBase = Number(vaultPerformance.data[0].realizedAPY)
+      }
+    } else if (APR) {
+      apyBase = Number(APR.rate)
+    }
+
+    const tvlUsd = Number(BN(vaultBlock.TVL.USD).div(1e06).toFixed(6))
+  
+    output.push({
+      pool: v.address,
+      apyBase,
+      apyReward,
+      rewardTokens,
+      symbol: token.symbol,
+      poolMeta: v.name,
+      tvlUsd,
+      project: 'idle',
+      chain: chain.name,
+      underlyingTokens: [token.address],
+    });
+  }
+
+  return output
 };
 
 module.exports = {
